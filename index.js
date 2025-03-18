dd19a98fefb20048b99e357841558d6cf679ce59
Larrymeez <meezlarry@gmail.com>
2025-03-18 22:11:52 +0300

    adds images to the file

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const ramens = [
     { id: 1, name: "Shoyu Ramen", restaurant: "Ichiran", image: "shoyu.jpg", rating: 5, comment: "Delicious!" },
-    { id: 2, name: "Miso Ramen", restaurant: "Menya", image: "miso.jpg", rating: 4, comment: "Very flavorful!" },
-    { id: 3, name: "Tonkotsu Ramen", restaurant: "Ramen-ya", image: "tonkotsu.jpg" }
+    { id: 2, name: "Miso Ramen", restaurant: "Menya", image: "nirvana.jpg", rating: 4, comment: "Very flavorful!" },
+    { id: 3, name: "Tonkotsu Ramen", restaurant: "Ramen-ya", image: "naruto.jpg" }
  ];
